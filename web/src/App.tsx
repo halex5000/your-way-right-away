37df1f2e61ff31568548ea5e4b9b2229fcd4818c
Alexander Hardman <alexander.hardman@gmail.com>
2022-12-07 13:19:45 -0500

    initial commit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,6 +4,7 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
+import 'antd/dist/reset.css'
 import './index.css'
 
 const App = () => (
